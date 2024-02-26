c5c33779893f9940f7539d03addcfdd2b9acf1b9
wenkai-li <wenkailileo@gmail.com>
2024-02-25 21:04:45 -0500

    get halueval result for istj and entp

--- llms_mbti/evaluation/hallucination_eval/HaluEval/evaluation/run_evaluation.sh
+++ llms_mbti/evaluation/hallucination_eval/HaluEval/evaluation/run_evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=llama2_13b
-#SBATCH --output=halueval_llama_13b_with_qa.out
+#SBATCH --output=halueval_llama_13b_with_qa_dpo_istj.out
 # SBATCH --error=llama13_vllm.err
 # SBATCH --partition=babel-shared
 #SBATCH --nodes=1
@@ -16,6 +16,6 @@ source ~/.bashrc
 conda activate llm_personality
 
 cd ~/llm_personality/llms_mbti/evaluation/hallucination_eval/HaluEval/evaluation
-python evaluate_with_vllm.py --task qa --model llama_13b_2 --model_path /data/datasets/models/huggingface/meta-llama/Llama-2-13b-chat-hf/ 
+python evaluate_with_vllm.py --task qa --model llama_13b_dpo_istj --model_path /data/datasets/models/huggingface/meta-llama/Llama-2-13b-chat-hf/ --lora True --lora_type dpo --lora_path /data/user_data/wenkail/llama_finetune_13b_persona/dpo_checkpoint/lora/istj/checkpoint-12000
 
-# --lora True --lora_type dpo --lora_path /data/user_data/wenkail/llama_finetune_13b_persona/dpo_checkpoint/lora/istj/checkpoint-12000
+# --lora True --lora_type dpo --lora_path /data/user_data/wenkail/llama_finetune_13b_persona/dpo_checkpoint/lora/enfp/checkpoint-15000
