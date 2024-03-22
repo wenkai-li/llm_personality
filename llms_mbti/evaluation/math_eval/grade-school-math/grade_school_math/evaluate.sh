c5073008fb60389107731e89782ab08ea25fc774
wenkai-li <wenkailileo@gmail.com>
2024-03-22 16:30:02 -0400

    gain result of original llama-13b

---
+++ llms_mbti/evaluation/math_eval/grade-school-math/grade_school_math/evaluate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# parameters
+MATHOD="zero_shot"
+MODEL="llama-13b"
+MODEL_PATH="/data/models/huggingface/meta-llama/Llama-2-13b-chat-hf"
+LORA_PATH="/data/user_data/wenkail/llama_finetune_13b_persona/dpo_checkpoint/lora/istj/checkpoint-12000"
+DATASET="gsm8k"
+
+
+source ~/.bashrc
+conda activate llm_personality
+cd ~/llm_personality/llms_mbti/evaluation/math_eval/grade-school-math/grade_school_math
+python evaluate.py --method=$MATHOD --model=$MODEL --dataset=$DATASET --model_path=$MODEL_PATH --with_lora True --lora_path $LORA_PATH
+
+
+# LLaMa-13b-unfinetuned
+# with zero-shot:
+# The accuracy is 6.1410159211523885%
+
+# LLaMa-13b-unfinetuned
+# with zero-shot-cot:
+# The accuracy is 28.582259287338896%
+
+# LLaMa-13b-dpo-finetuned-checkpoint-12000-istj
+# with zero-shot:
