c0382982ef463fbaae44e3b2a27a1ecf4b489b67
Jiarui Liu <ljr-jerry@sjtu.edu.cn>
2024-06-18 00:46:27 -0400

    upload classifier model inference code

--- llm_bigfive/classifier/roberta_classifier.py
+++ llm_bigfive/classifier/roberta_classifier.py
@@ -17,23 +17,10 @@ logger = logging.getLogger(__name__)
 logger.info("Logging setup complete.")
 
 ###########
-# Save each dataset to a separate directory
+## Save each dataset to a separate directory
+# from utils import preprocess_function
 # df = pd.read_csv('big5_training_data_with_token_roberta.csv')
 # # df = df.sample(frac=0.01, random_state=42)
-
-# def preprocess_function(examples):
-#     if isinstance(examples['message_tokens'][0], str):
-#         input_ids = [ast.literal_eval(tokens) for tokens in examples['message_tokens']]
-#     else:
-#         input_ids = examples['message_tokens']
-    
-#     max_length = 512
-#     input_ids = [tokens[:max_length] + [0] * (max_length - len(tokens)) for tokens in input_ids]
-
-#     labels = torch.tensor([[examples[col + "_level"][i] for col in ['ope_z', 'con_z', 'ext_z', 'agr_z', 'neu_z']]
-#                            for i in range(len(examples['message_tokens']))], dtype=torch.float)
-    
-#     return {'input_ids': input_ids, 'labels': labels}
 
 # dataset = Dataset.from_pandas(df.dropna())
 # dataset = dataset.map(preprocess_function, batched=True)
@@ -54,7 +41,8 @@ num_labels = 1 # we have already defined the sub-loss for each personality dimen
 model = RobertaForSequenceClassification.from_pretrained("roberta-large", num_labels=num_labels, cache_dir="/data/user_data/jiaruil5/.cache")
 
 training_args = TrainingArguments(
-    output_dir="/data/user_data/wenkail/llm_personality/classifier/roberta/ljr/test/",
+    # output_dir="/data/user_data/wenkail/llm_personality/classifier/roberta/ljr/test/",
+    output_dir="/data/user_data/wenkail/llm_personality/classifier/roberta/ljr/tmp/",
     evaluation_strategy="steps",
     eval_steps=500,
     learning_rate=1e-4,
@@ -83,15 +71,6 @@ trainer = Trainer(
 )
 
 trainer.train()
-
-model.save_pretrained('/data/user_data/wenkail/llm_personality/classifier/roberta/model_4')
-
-tokenizer = RobertaTokenizer.from_pretrained("roberta-large")
-inputs = tokenizer("Predict personality scores: Example input text here", return_tensors="pt")
-outputs = model(**inputs)
-predictions = outputs.logits
-print(predictions)
-
 
 # O, C, E, A, N
 # [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
