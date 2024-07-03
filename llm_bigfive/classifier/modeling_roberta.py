e040a0ac525930c391aa325e6fe1cb99a9b17c96
Jiarui Liu <ljr-jerry@sjtu.edu.cn>
2024-07-03 01:17:39 -0400

    update huggingface trainer predict file

--- llm_bigfive/classifier/modeling_roberta.py
+++ llm_bigfive/classifier/modeling_roberta.py
@@ -94,7 +94,8 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
         # average over five MSE losses (o, c, e, a, n)
         # logger.info(f"Training sub-loss: O {loss[0]}, C {loss[1]}, E {loss[2]}, A {loss[3]}, N {loss[4]}")
         loss = (loss[0] + loss[1] + loss[2] + loss[3] + loss[4]) / 5
-        
+        if not isinstance(loss, torch.Tensor):
+            loss = torch.tensor(loss)
         if not return_dict:
             output = (logits,) + outputs[2:]
             return ((loss,) + output) if loss is not None else output
