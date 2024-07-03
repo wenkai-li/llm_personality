7cb45c9a056664f517b064d9291d9be5de28d0aa
Jiarui Liu <ljr-jerry@sjtu.edu.cn>
2024-07-03 00:36:38 -0400

    update utils

--- llm_bigfive/classifier/utils.py
+++ llm_bigfive/classifier/utils.py
@@ -2,21 +2,6 @@ import numpy as np
 import torch
 import ast
 
-def preprocess_function(examples):
-    # mse loss
-    if isinstance(examples['message_tokens'][0], str):
-        input_ids = [ast.literal_eval(tokens) for tokens in examples['message_tokens']]
-    else:
-        input_ids = examples['message_tokens']
-    
-    max_length = 512
-    input_ids = [tokens[:max_length] + [0] * (max_length - len(tokens)) for tokens in input_ids]
-
-    labels = torch.tensor([[examples[col + "_level"][i] for col in ['ope_z', 'con_z', 'ext_z', 'agr_z', 'neu_z']]
-                           for i in range(len(examples['message_tokens']))], dtype=torch.float)
-    
-    return {'input_ids': input_ids, 'labels': labels}
-
 def preprocess_function_with_tokenizer(examples, tokenizer):
     messages = examples['message']
     
