0ae162039d952bc6ca2d16048c84c0838d2ba893
Jiarui Liu <ljr-jerry@sjtu.edu.cn>
2024-07-03 01:17:39 -0400

    update huggingface trainer predict file

--- llm_bigfive/classifier/utils.py
+++ llm_bigfive/classifier/utils.py
@@ -15,6 +15,16 @@ def preprocess_function_with_tokenizer(examples, tokenizer):
     
     return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
 
+def preprocess_function_with_tokenizer_without_labels(examples, tokenizer):
+    messages = examples['message']
+    
+    tokenized_messages = tokenizer(messages, truncation=True, padding='max_length', max_length=512)
+    
+    input_ids = tokenized_messages['input_ids']
+    attention_mask = tokenized_messages['attention_mask']
+    
+    return {'input_ids': input_ids, 'attention_mask': attention_mask}
+
 def to_one_hot(value, num_classes):
     one_hot = np.zeros(num_classes, dtype=int)
     one_hot[value] = 1
