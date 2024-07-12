1b3454102577158fbcac1e0b5f9e57b5f9fb7c7f
wenkai-li <wenkailileo@gmail.com>
2024-07-12 12:39:38 -0400

    add expert generator generated test set result

--- llm_bigfive/llama_dexpert/evaluate_llama_inference.py
+++ llm_bigfive/llama_dexpert/evaluate_llama_inference.py
@@ -11,25 +11,23 @@ import pdb
 
 def llm_config_func(llm):
     llm.temperature = 0
-    llm.max_tokens = 4096
+    llm.max_tokens = 512
     return llm
 
-
-def get_prediction_list(model, testset, config):
-    prediction = []
+def get_prediction_and_write(model, testset, config, output_file):
     with open(testset) as f:
         testset = json.load(f)
 
-    # Subsample the testset
-    # num_samples = 1000
-    # seed = 42
-    # random.seed(seed)
-    # sub_testset = random.sample(testset, num_samples)
-    # pdb.set_trace()
-    for test in tqdm(testset):
+    try:
+        with open(output_file, 'r') as f:
+            processed_count = sum(1 for line in f if line.strip())
+    except FileNotFoundError:
+        processed_count = 0
+
+    for test in tqdm(testset[processed_count:], initial=processed_count, total=len(testset)):
         predict_dict = {}
         prompt = [
-        f"{test['instruction']} {test["input"]} \n"]
+        f"{test['instruction']} {test['input']} \n"]
         res = call(
             prompt,
             llm_config_func,
@@ -38,28 +36,25 @@ def get_prediction_list(model, testset, config):
             verbose=True,
             **config
         )
-        predict_dict['instuction'] = test['instruction']
+        predict_dict['instruction'] = test['instruction']
         predict_dict['input'] = test['input']
         predict_dict['output'] = res
-        prediction.append(predict_dict)
-    return prediction
+        
+        with open(output_file, 'a') as f:
+            json.dump(predict_dict, f, indent=4)
+            f.write('\n') 
 
 def main():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("--model_name", default=None, type=str, required=True, help = "Choose the evlauate model, the name can be seen in config")
-    parser.add_argument("--testset", default="/data/user_data/wenkail/llm_personality/generator/data/alpaca_big_five_dataset_test_5_tokens.json", type=str, required=False, help="Give a absolute path for testset")
+    parser.add_argument("--model_name", default=None, type=str, required=True, help="Choose the evaluate model, the name can be seen in config")
+    parser.add_argument("--testset", default="/data/user_data/wenkail/llm_personality/generator/data/alpaca_big_five_dataset_test_5_tokens.json", type=str, required=False, help="Give an absolute path for testset")
     parser.add_argument("--output_file", default=None, type=str, required=True, help="The output file path and name")
     args = parser.parse_args()
 
     config = get_model_config(args.model_name)
-    predictions = get_prediction_list(args.model_name, args.testset, config)
-    
-    with open(args.output_file, 'w') as f:
-        json.dump(predictions, f, indent=4)
+
+    get_prediction_and_write(args.model_name, args.testset, config, args.output_file)
 
 if __name__ == "__main__":
     main()
-
-        
-
