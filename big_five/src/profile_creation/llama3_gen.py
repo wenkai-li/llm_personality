19626fb858c5ba5989d5e02845342125549e8d86
Jiarui Liu <ljr-jerry@sjtu.edu.cn>
2024-07-19 15:08:55 -0400

    update code

--- big_five/src/profile_creation/llama3_gen.py
+++ big_five/src/profile_creation/llama3_gen.py
@@ -27,24 +27,16 @@ class CO3Sotopia():
         class ArgsExpert:
             # model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
             # cache_dir = "/data/user_data/jiaruil5/.cache/"
-            model_id = "/compute/babel-5-23/jiaruil5/personality/checkpoints/word5_lr1e-5/checkpoint-3000/"
+            model_id = "/compute/babel-0-37/jiaruil5/personality/checkpoints/generator_whole_1e-6/checkpoint-9000/"
             cache_dir = None
 
         self.model = DExpertGenerator(args=Args, args_expert=ArgsExpert)
         self.data = pd.read_csv(args.in_file).to_dict(orient='records')
-        fifth = len(self.data) // 5
-        if args.chunk == "1/5":
-            self.data = self.data[:fifth]
-        elif args.chunk == "2/5":
-            self.data = self.data[fifth:2*fifth]
-        elif args.chunk == "3/5":
-            self.data = self.data[2*fifth:3*fifth]
-        elif args.chunk == "4/5":
-            self.data = self.data[3*fifth:4*fifth]
-        elif args.chunk == "5/5":
-            self.data = self.data[4*fifth:]
-        else:
-            raise NotImplementedError()
+        curr_idx, total_idx = args.chunk.split("/")
+        curr_idx = int(curr_idx)
+        total_idx = int(total_idx)
+        parts = len(self.data) // total_idx
+        self.data = self.data[(curr_idx-1)*parts:curr_idx*parts]
         
     
     def process_response(self, response):
@@ -149,7 +141,7 @@ class CO3Sotopia():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for generating dialogues')
     parser.add_argument("--in_file", type=str, default="/data/user_data/wenkail/llm_personality/soda_data/sample_10000.csv", help="The file of the sampled soda training data")
-    parser.add_argument("--out_file", type=str, default="/data/user_data/wenkail/llm_personality/profiles/env_profiles.jsonl")
+    parser.add_argument("--out_file", type=str, default="/data/user_data/wenkail/llm_personality/profiles/env_profiles_new.jsonl")
     parser.add_argument("--alpha", type=float, default=0.5)
     parser.add_argument("--chunk", type=str, default="1/5")
     args = parser.parse_args()
