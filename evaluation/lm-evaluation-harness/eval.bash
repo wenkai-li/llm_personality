339105d14d6f58fc2eb5e716305e414321fbcd5b
wenkai-li <wenkailileo@gmail.com>
2024-09-10 02:42:07 -0400

    add eval.bash and modify user prompt

--- evaluation/lm-evaluation-harness/eval.bash
+++ evaluation/lm-evaluation-harness/eval.bash
@@ -9,6 +9,7 @@ echo "Activating lm_eval environment..."
 conda activate lm_eval
 
 echo "Do the Llama 3 8B GSM8K 5 Shots Evaluation"
+MODEL_PATH=""
 FILE_NAME="llama3_8b_gsm8k_5_shots_without_cot.json"
 FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks gsm8k --model_args pretrained=/data/models/huggingface/meta-llama/Meta-Llama-3-8B-Instruct,parallelize=True --batch_size 16 --apply_chat_template --result=$FULL_PATH
+CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks gsm8k --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size 16 --apply_chat_template --output_path $FULL_PATH
