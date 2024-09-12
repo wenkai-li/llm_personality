dbbfa0ec098939f32d4be854f976ed82b8aa11bd
wenkai-li <wenkailileo@gmail.com>
2024-09-12 14:47:15 -0400

    add llama 3 8b eval results

--- evaluation/lm-evaluation-harness/bash_scripts/dpo/llama3_8b_eval.bash
+++ evaluation/lm-evaluation-harness/bash_scripts/dpo/llama3_8b_eval.bash
@@ -9,33 +9,33 @@ source ~/.bashrc
 echo "Activating lm_eval environment..."
 conda activate lm_eval
 
-# Social reasoning benchmark
-echo "Do the Llama 3 8B SocialIQA Evaluation"
-TASK_NAME="social_iqa"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH
+# # Social reasoning benchmark
+# echo "Do the Llama 3 8B SocialIQA Evaluation"
+# TASK_NAME="social_iqa"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH
 
-# General Benchmark
-# General:
-echo "Do the Llama 3 8B MMLU Evaluation"
-TASK_NAME="mmlu"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# # General Benchmark
+# # General:
+# echo "Do the Llama 3 8B MMLU Evaluation"
+# TASK_NAME="mmlu"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
-# Commonsing Reasoning
-echo "Do the Llama 3 8B PIQA Evaluation"
-TASK_NAME="piqa"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# # Commonsing Reasoning
+# echo "Do the Llama 3 8B PIQA Evaluation"
+# TASK_NAME="piqa"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
-echo "Do the Llama 3 8B CommonsenseQa Evaluation"
-TASK_NAME="commonsense_qa"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# echo "Do the Llama 3 8B CommonsenseQa Evaluation"
+# TASK_NAME="commonsense_qa"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
 echo "Do the Llama 3 8B GPQA Main Zero Shot Evaluation"
 TASK_NAME="gpqa_main_zeroshot"
@@ -61,25 +61,25 @@ FILE_NAME="llama3_8b_${TASK_NAME}"
 FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
 CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
-# Math Reasoning
-echo "Do the Llama 3 8B GSM8K 5 Shots Evaluation"
-TASK_NAME="gsm8k"
-FILE_NAME="llama3_8b_${TASK_NAME}_5_shots_without_cot"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True,peft=$LORA_PATH --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# # Math Reasoning
+# echo "Do the Llama 3 8B GSM8K 5 Shots Evaluation"
+# TASK_NAME="gsm8k"
+# FILE_NAME="llama3_8b_${TASK_NAME}_5_shots_without_cot"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True,peft=$LORA_PATH --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
-echo "Do the Llama 3 8B MathQA Evaluation"
-TASK_NAME="mathqa"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# echo "Do the Llama 3 8B MathQA Evaluation"
+# TASK_NAME="mathqa"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
 
-# Safety
-echo "Do the Llama 3 8B Truthful QA Evaluation"
-TASK_NAME="truthfulqa"
-FILE_NAME="llama3_8b_${TASK_NAME}"
-FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
+# # Safety
+# echo "Do the Llama 3 8B Truthful QA Evaluation"
+# TASK_NAME="truthfulqa"
+# FILE_NAME="llama3_8b_${TASK_NAME}"
+# FULL_PATH="${BASE_DIRECTORY}/${FILE_NAME}"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model hf --tasks $TASK_NAME --model_args pretrained=$MODEL_PATH,parallelize=True --batch_size $BATCH_SIZE --apply_chat_template --output_path $FULL_PATH 
 
 
