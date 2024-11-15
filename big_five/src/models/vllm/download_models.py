a1325eeb66575d584a99da55ec99dcff1013996c
wenkai-li <wenkailileo@gmail.com>
2024-11-15 17:05:26 -0500

    update psychobench results

--- big_five/src/models/vllm/download_models.py
+++ big_five/src/models/vllm/download_models.py
@@ -1,17 +1,11 @@
 import transformers
 import torch
 
-# model_name = "meta-llama/Llama-2-7b-hf"
-# model_name = "meta-llama/Llama-2-13b-chat-hf"
-access_token = "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"
 
-# model_name = "lmsys/vicuna-13b-v1.5"
-# model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+access_token = ""
+
 model_name = "TechxGenus/Meta-Llama-3-70B-Instruct-GPTQ"
 
-# model_name = "chavinlo/alpaca-native"
-
-# cache_dir = "/data/user_data/jiaruil5/.cache"
 cache_dir = "/scratch/wenkail/.cache"
 
 tokenizer = transformers.AutoTokenizer.from_pretrained(
