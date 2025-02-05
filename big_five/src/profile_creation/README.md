fcc7f94008d7ff4774f56b4ea9bf7e37d1568811
wenkai-li <wenkailileo@gmail.com>
2025-02-05 11:38:51 -0500

    add finetune

--- big_five/src/profile_creation/README.md
+++ big_five/src/profile_creation/README.md
@@ -34,3 +34,20 @@ python3 llama3_gen.py --out_file /data/user_data/wenkail/llm_personality/profile
 
 python3 llama3_gen.py --out_file /data/user_data/wenkail/llm_personality/profiles/env_profiles_n_1_new.jsonl --alpha 0.5 --chunk 1/2 --person_trait n
 python3 llama3_gen.py --out_file /data/user_data/wenkail/llm_personality/profiles/env_profiles_n_2_new.jsonl --alpha 0.5 --chunk 2/2 --person_trait n
+
+
+## wenkai
+/home/wenkail/iclr_rebuttal/llm_personality/big_five/src/profile_creation
+
+CUDA_VISIBLE_DEVICES=0 python llama3_gen_finetune.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/sft_o_1.jsonl --chunk 1/10 --person_trait o
+CUDA_VISIBLE_DEVICES=1 python llama3_gen_finetune.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/sft_c_1.jsonl --chunk 1/10 --person_trait c
+CUDA_VISIBLE_DEVICES=2 python llama3_gen_finetune.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/sft_e_1.jsonl --chunk 1/10 --person_trait e
+CUDA_VISIBLE_DEVICES=3 python llama3_gen_finetune.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/sft_a_1.jsonl --chunk 1/10 --person_trait a
+
+
+
+/home/wenkail/iclr_rebuttal/llm_personality/big_five/src/profile_creation
+CUDA_VISIBLE_DEVICES=0 python llama3_gen_finetune_without_lora.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/prompt_o_1.jsonl --chunk 1/10 --person_trait o
+CUDA_VISIBLE_DEVICES=1 python llama3_gen_finetune_without_lora.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/prompt_c_1.jsonl --chunk 1/10 --person_trait c
+CUDA_VISIBLE_DEVICES=2 python llama3_gen_finetune_without_lora.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/prompt_e_1.jsonl --chunk 1/10 --person_trait e
+CUDA_VISIBLE_DEVICES=3 python llama3_gen_finetune_without_lora.py --out_file /data/user_data/wenkail/llm_personality/profiles/finetune_soda/prompt_a_1.jsonl --chunk 1/10 --person_trait a
---
+++ big_five/src/profile_creation/llama3_gen_finetune_without_lora.py
@@ -0,0 +1,251 @@
+import json
+import sys
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import argparse
+import re
+from peft import PeftModel
+import torch
+np.random.seed(42)
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from prompts import generate_prompt
+sys.path.append("../dexpert/")
+from modeling_llama import LlamaForCausalLM
+
+def remove_double_quotes(s):
+    # Remove double quotes from the beginning and end
+    return re.sub(r'^"+|"+$', '', s)
+
+
+class Generator():
+    def __init__(self, args):
+        
+        self.args = args
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=args.model_id,
+            padding_side="left",
+            cache_dir=args.cache_dir
+        )
+        self.terminators = [
+            self.tokenizer.eos_token_id,
+            self.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+        
+        self.model = LlamaForCausalLM.from_pretrained(
+            args.model_id,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            cache_dir=args.cache_dir
+        )
+        # if args.lora:
+        #     self.model = PeftModel.from_pretrained(
+        #         self.model,
+        #         args.lora_model_path
+        #     )
+        self.model.eval()
+        
+    def generate_conv(self, messages):
+        """
+        Call the normal generate function without
+        """
+        input_ids = self.tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(self.model.device)
+        
+        outputs = self.model.generate(
+            input_ids,
+            max_new_tokens=1024,
+            eos_token_id=self.terminators,
+            do_sample=False
+        )
+        response = outputs[0][input_ids.shape[-1]:]
+        result = self.tokenizer.decode(response, skip_special_tokens=True)
+        return result
+    
+    def generate(self, messages, messages_expert=None, messages_antiexpert=None, alpha=None):
+        """
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt1},
+            
+            {"role": "assistant", "content": response2},
+            {"role": "user", "content": prompt1},
+            
+            {"role": "assistant", "content": response2},
+        ]
+        
+        DExpert only has effect on assistant responses.
+        """
+
+        input_ids = self.tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(self.model.device)
+        
+        input_ids_expert = self.tokenizer.apply_chat_template(
+            messages_expert,
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(self.model.device) if messages_expert is not None else None
+        
+        input_ids_antiexpert = self.tokenizer.apply_chat_template(
+            messages_antiexpert,
+            add_generation_prompt=True,
+            return_tensors="pt"
+        ).to(self.model.device) if messages_antiexpert is not None else None
+
+        outputs = self.model.generate(
+            input_ids,
+            input_ids_expert=input_ids_expert,              # dexpert
+            input_ids_antiexpert=input_ids_antiexpert,      # dexpert
+            alpha=alpha,                                    # dexpert
+            max_new_tokens=1024,
+            eos_token_id=self.terminators,
+            do_sample=False
+        )
+        response = outputs[0][input_ids.shape[-1]:]
+        result = self.tokenizer.decode(response, skip_special_tokens=True)
+        return result
+        
+class CO3Sotopia():
+    def __init__(self, args):
+        self.args = args
+        
+        class Args:
+            model_id = "/compute/babel-0-37/wenkail/.cache/models--TechxGenus--Meta-Llama-3-70B-Instruct-GPTQ/snapshots/e147aa8799dd05d5077f60c79be0d972b002b3ac"
+            cache_dir = None
+            lora_model_path = "/data/user_data/wenkail/llm_personality/align/70b_gptq_lora_sft_1e-5/checkpoint-1200"
+            lora = True
+            
+        self.model = Generator(args=Args)
+        self.data = pd.read_csv(args.in_file).to_dict(orient='records')
+        
+        curr_idx, total_idx = args.chunk.split("/")
+        curr_idx = int(curr_idx)
+        total_idx = int(total_idx)
+        parts = len(self.data) // total_idx
+        self.data = self.data[(curr_idx-1)*parts:curr_idx*parts]
+        
+    
+    def process_response(self, response):
+        try:
+            # split by \n\n and get the middle one?
+            if ":\n\n" in response:
+                response = response.split(":\n\n")
+                response = response[-1]
+            # remove quotation marks at the beginning and the end
+            response = remove_double_quotes(response)
+            return response
+        except Exception as e:
+            print("Error during processing response", response)
+            return response
+        
+    def generate_messages(self, prompt):
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+        return messages
+    
+    def generate_expert_messages(self, big_five_level):
+        level_lst = ['high', 'low']
+        if big_five_level[0] != -1:
+            prompt_person_str = f"Openness - {level_lst[big_five_level[0]]}"
+        elif big_five_level[1] != -1:
+            prompt_person_str = f"Conscientiousness - {level_lst[big_five_level[1]]}"
+        elif big_five_level[2] != -1:
+            prompt_person_str = f"Extraversion - {level_lst[big_five_level[2]]}"
+        elif big_five_level[3] != -1:
+            prompt_person_str = f"Agreeableness - {level_lst[big_five_level[3]]}"
+        elif big_five_level[4] != -1:
+            prompt_person_str = f"Neuroticism - {level_lst[big_five_level[4]]}"
+        
+        prompt = f"You are a helpful assistant with the following Big Five personality traits: {prompt_person_str}\n"
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+        return messages
+    
+    def generate_dialogue_turn0(self, idx, env_info, out_f):
+        # generate prompt for turn 0
+        self.prompt_turn_0 = generate_prompt(
+            env_info,
+            current_turn_index=0,
+            # p1_personality_and_values=p1_big_five,
+            # p2_personality_and_values=p2_big_five,
+        )
+        
+        self.response_turn_0 = self.process_response(self.model.generate(
+            messages = self.generate_messages(self.prompt_turn_0),
+            alpha = self.args.alpha,
+        ))
+        
+        result_info = {
+            "env_idx": idx,
+            "env_info": env_info,
+            "turn": 0,
+            "prompt": self.prompt_turn_0,
+            "response": self.response_turn_0,
+        }
+        json.dump(result_info, out_f)
+        out_f.write("\n")
+        out_f.flush()
+    
+    def generate_dialogue_turn1(self, idx, env_info, p2_big_five, out_f):
+        
+        
+        # generate prompt for turn 1
+        prompt_turn_1 = generate_prompt(
+            env_info,
+            current_turn_index=1,
+            # p1_personality_and_values=p1_big_five,
+            p2_personality_and_values=p2_big_five,
+            p1_argument = self.response_turn_0,
+        )
+        
+        response_turn_1 = self.process_response(self.model.generate(
+            messages = self.generate_messages(prompt_turn_1),
+            alpha = self.args.alpha,
+        ))
+        
+        result_info = {
+            "env_idx": idx,
+            "env_info": env_info,
+            "personality": " ".join(map(str, p2_big_five)),
+            "turn": 1,
+            "prompt": prompt_turn_1,
+            "response": response_turn_1,
+        }
+        json.dump(result_info, out_f)
+        out_f.write("\n")
+        out_f.flush()
+    
+    def run(self):
+        out_f = open(self.args.out_file, 'a')
+        p2_big_five_ref = [-1, -1, -1, -1, -1]
+        p2_big_five_abbr = {'o': 0, 'c': 1, 'e': 2, 'a': 3, 'n': 4}
+        
+        p2_big_five_high, p2_big_five_low = p2_big_five_ref.copy(), p2_big_five_ref.copy()
+        p2_big_five_high[p2_big_five_abbr[self.args.person_trait]] = 0 # high
+        p2_big_five_low[p2_big_five_abbr[self.args.person_trait]] = 1 # low
+        
+        for idx, env_info in tqdm(enumerate(self.data)):
+            self.generate_dialogue_turn0(idx, env_info, out_f)
+            self.generate_dialogue_turn1(idx, env_info, p2_big_five_high, out_f)
+            self.generate_dialogue_turn1(idx, env_info, p2_big_five_low, out_f)
+            
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='arguments for generating dialogues')
+    parser.add_argument("--in_file", type=str, default="/data/user_data/wenkail/llm_personality/soda_data/sample_10000.csv", help="The file of the sampled soda training data")
+    parser.add_argument("--out_file", type=str, default="/data/user_data/wenkail/llm_personality/profiles/env_profiles.jsonl")
+    parser.add_argument("--alpha", type=float, default=0.5)
+    parser.add_argument("--person_trait", type=str, choices=['o', 'c', 'e', 'a', 'n'])
+    parser.add_argument("--chunk", type=str, default="1/2")
+    args = parser.parse_args()
+    soda_maker = CO3Sotopia(args)
+    soda_maker.run()
